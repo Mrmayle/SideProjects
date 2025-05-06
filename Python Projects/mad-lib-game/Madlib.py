9b806dcd32db75b6983f2ad4c9f50817413f935e
Mrmayle <Markusmayle@gmail>
2025-05-06 12:52:55 -0400

    added old projects

---
+++ Python Projects/mad-lib-game/Madlib.py
@@ -0,0 +1,14 @@
+#youtuber = "Babymayle"
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format (youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input ("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programing is so {adj}! It makes me so excited all the time to because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print (madlib)
+
