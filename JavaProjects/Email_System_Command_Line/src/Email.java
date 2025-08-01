56bdca97646f79104b3d103794fe36e468b610a9
Markus Mayle <markusmayle@gmail.com>
2025-07-31 22:36:50 -0400

    Added random password method and scanner input options

--- JavaProjects/Email_System_Command_Line/src/Email.java
+++ JavaProjects/Email_System_Command_Line/src/Email.java
@@ -1,14 +1,29 @@
+import java.util.Scanner;
+
 public class Email {
     private String firstname;
     private String lastname;
     private String password;
     private String department;
+    private String email;
     private String alternateEmail;
-    private int mailboxCapacity;
+    private int mailboxCapacity = 500;
+    private int defaultPasswordLength = 8;
+    private String companySuffix = "mvmindustries";
 
     public Email(String firstname, String lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
+        System.out.println("Email created: " + this.firstname + " " + this.lastname);
+        // setting department
+        this.department = setEmailDepartment();
+        System.out.println("Department: " + this.department);
+        // set random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Your temporary password is: " + this.password);
+        //create email
+        email = firstname.toLowerCase() + "." + lastname.toLowerCase() + "@" + department + "." + companySuffix + ".com";
+        System.out.println("Your new company email address is: " + email);
     }
 
     public Email(String firstname, String lastname, String password, String department, String alternateEmail, int mailboxCapacity) {
@@ -18,6 +33,36 @@ public class Email {
         this.department = department;
         this.alternateEmail = alternateEmail;
         this.mailboxCapacity = mailboxCapacity;
+    }
+
+    private String setEmailDepartment(){
+        System.out.println("Select Department Code\n1 Sales\n2 Development\n3 Accounting\n4 HR\n5 DevOps\n0 for None \nEnter Selection:");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+
+        if (depChoice == 1){
+            return "sales";
+        } else if (depChoice == 2) {
+            return "development";
+        } else if (depChoice == 3) {
+            return "accounting";
+        } else if (depChoice == 4) {
+            return "hr";
+        } else if (depChoice == 5) {
+            return "devops";
+        } else
+            return "";
+
+    }
+
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$";
+        char[] password = new char[length];
+        for(int i=0; i<length; i++) {
+            int rando = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rando);
+        }
+        return new String(password);
     }
 
     public String getFirstname() {
